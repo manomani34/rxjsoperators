890929f7994bbcea510424d5ff793439f1aa6695
Name <Email>
2020-10-04 19:47:13 +0330

    home
    home end

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Observable, Subscription } from 'rxjs';
+import { Observable, Subject, Subscription } from 'rxjs';
 import 'rxjs/Rx';
 
 
@@ -15,36 +15,66 @@ export class AppComponent implements OnInit, OnDestroy {
 
   title = 'rxjsoperators';
   subscription: Subscription;
+  searchTern = new Subject<string>();
 
   ngOnInit(): void {
+    // **map
     // const source = Observable.from([1, 2, 3, 4, 5, 6, 12, 8])
     //   .map((x) => {
     //       return x * 2;
     //   });
 
+    // **filter
     // const source = Observable.from([1, 2, 3, 4, 5, 6, 12, 8])
     // .filter((x) => {
     //     return x % 2 == 0;
     // });
 
+    // **interval
     // const source = Observable.interval(2000)
     // .map( (x) => {
     //     return (x * 2) + 'ddd'       
     //   }
     // )
-    // const source = Observable.fromEvent(document , 'click')
-    const source = Observable.of('mammad', 'baheri')
-      .do(x => console.log('befor map', x))
-      .map(x => x.toUpperCase())
-      .map(x => console.log('afer map', x));
 
-    this.subscription = source.subscribe(
-      // (x) => {
-      //   console.log(x);
-      // }
-    )
+    // **fromEvent
+    // const source = Observable.fromEvent(document , 'click')
+
+    // **do
+    // const source = Observable.of('mammad', 'baheri')
+    //   .do(x => console.log('befor map', x))
+    //   .map(x => x.toUpperCase())
+    //   .map(x => console.log('afer map', x));
+
+    // this.subscription = source.subscribe(
+    // (x) => {
+    //   console.log(x);
+    // }
+    // )
+
+    // **debounceTime
+    //this.searchTern.debounceTime(2000).subscribe(
+    //   this.searchTern.throttleTime(2000).subscribe(
+    //   (x) => {
+    //     console.log (x);
+    //   }
+    // );
+
+    // **forkJoin
+    //  const obs1 = Observable.of('ddd' , 'uuu' , 'yyy')
+    //  let obs2 = Observable.of('123' , '456' , '789')
+    //  const summery = Observable.forkJoin(obs1 , obs2)
+    //  obs2 = obs2.delay(5000);
+    //  summery.subscribe((x) => {console.log(x)})
+
+    // **distinctUntilChanged
+    const myArry = Observable.from([5, 5, 1, 1, 5, 2, 2, 8, 8, 8, 8, 9, 9, 8, 8, 7, 7]);
+    const distinct = myArry.distinctUntilChanged().subscribe(x => console.log(x));
   }
 
+  txt() {
+
+  }
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
